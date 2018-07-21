85c498543516375d7a7f4a472a221bccd3e19426
Govind <govindsaini442@gmail.com>
2018-07-21 08:19:45 +0530

    DB Connectivity Demo using python

---
+++ DB_Connect.py
@@ -0,0 +1,16 @@
+import cx_Oracle
+host="localhost"
+port = 1521
+SID = "orcl"
+user="system"
+passwd="redhat"
+dsn_tns = cx_Oracle.makedsn(host, port, SID)
+print(dsn_tns)
+
+con = cx_Oracle.connect(user, passwd, dsn_tns)
+print(con.version)
+cur = con.cursor()
+cur.execute("select * from TAB")
+for result in cur:
+    print(result)
+con.close()
