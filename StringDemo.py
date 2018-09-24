afc60353af7e4a0a4b23609b6173437f59fd4bc1
Govind <govindsaini442@gmail.com>
2018-09-24 20:24:35 +0530

    StringDemo.py added to project

--- StringDemo.py
+++ StringDemo.py
@@ -13,10 +13,47 @@ print(a.split()[1]) ## print Second string
 
 
 ####Command line string argument
-x = input("Enter a string:")
-print("You Entered: ",x)
+#x = input("Enter a string:")
+#print("You Entered: ",x)
 
 ## String addition
 str1 = "Hello"
 str2 = "Python"
 print(str1 +" "+ str2)
+
+## Iterate through a string and find out specific duplicate character
+count = 0
+for letter in 'Hello World':
+    if(letter == 'l'):
+        count += 1
+print(count,'letters found')
+
+##How to find duplicate character in a string
+test = "fhgjfhgjfhgjfhgjf"
+list = []
+listTmp = []
+for char in test:     ## interating thorugh the string
+    list.append(char) ## Adding element in a List
+print(list)
+
+for element in list:  ## Iterating element of the newly created list
+    #print(element)
+    if element in listTmp:
+        pass
+    else:
+        print("Total Count for",element+":",list.count(element))
+    listTmp.append(element)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
