2b9b63c4a7abba1653f21beef4c338313dcd9c78
Govind <govindsaini442@gmail.com>
2018-09-24 18:52:14 +0530

    Numbers.py added to project

---
+++ Numbers.py
@@ -0,0 +1,44 @@
+##There are three numeric types in Python:
+
+## 1) int
+## 2) float
+## 3) complex
+
+print("Python Numbers demo")
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(type(x))
+print(type(y))
+print(type(z))
+print("\n")
+
+####Int
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+print("\n")
+
+###Float
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+print("\n")
+
+##Complex
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
