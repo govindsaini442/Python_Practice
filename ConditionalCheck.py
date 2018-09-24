448d8137aa1a3ea0256b6927810314a04beb9940
Govind <govindsaini442@gmail.com>
2018-09-24 18:42:12 +0530

    ConditionalCheck.py added to project

---
+++ ConditionalCheck.py
@@ -0,0 +1,47 @@
+string = "Awesome"
+print("Python is",string)
+
+x = "Python is "
+y = "Awesome"
+z = x + y
+print(z)
+
+print("If loop Demo")
+# If loop Demo
+x = 10
+y = 20
+if x > y:
+    print("X is greater than Y ")
+else:
+    print("Y is greater than X")
+print("Done")
+print("\n")
+
+print("While loop demo")
+### While Loop
+Count = 0
+while Count < 10:
+    print(Count)
+    Count = Count+1
+print("Done")
+print("\n")
+
+## while loop with Break
+
+print("Break keyword demo")
+count = 0
+while count < 10:
+    print(count)
+    count = count+1
+    if count == 6:
+        break
+print("Done")
+print("\n")
+
+####For loop
+print("IF loop Demo")
+list1 = ["John","Sunil","Rock","Payne"]
+for word in list1:
+    print(word)
+print("Done")
+print("\n")
